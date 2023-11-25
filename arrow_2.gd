2758319da65999b5f403ca44c1fadadd20d18244
SHKHOR <140292840+SHKHOR@users.noreply.github.com>
2023-11-25 08:55:37 +0800

    Add files via upload

---
+++ arrow_2.gd
@@ -0,0 +1,10 @@
+extends AnimatableBody2D
+
+
+func _ready():
+	$AnimationPlayer.play("shoot")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
